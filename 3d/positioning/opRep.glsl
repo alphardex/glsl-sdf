b5a885fb460f978b0611535aad417834c1bd0d57
alphardex <2582347430@qq.com>
2022-04-13 09:55:30 +0800

    update repeat function

--- 3d/positioning/opRep.glsl
+++ 3d/positioning/opRep.glsl
@@ -1,4 +1,14 @@
-vec3 opRep(vec3 p,vec3 c)
+float opRep(in float p,in float c)
+{
+    return mod(p,c)-.5*c;
+}
+
+vec2 opRep(in vec2 p,in vec2 c)
+{
+    return mod(p,c)-.5*c;
+}
+
+vec3 opRep(in vec3 p,in vec3 c)
 {
     return mod(p,c)-.5*c;
 }
--- 3d/positioning/opRepLim.glsl
+++ 3d/positioning/opRepLim.glsl
@@ -3,4 +3,14 @@ float opRepLim(in float p,in float s,in float lima,in float limb)
     return p-s*clamp(round(p/s),lima,limb);
 }
 
+vec2 opRepLim(in vec2 p,in float s,in vec2 lima,in vec2 limb)
+{
+    return p-s*clamp(round(p/s),lima,limb);
+}
+
+vec3 opRepLim(in vec3 p,in float s,in vec3 lima,in vec3 limb)
+{
+    return p-s*clamp(round(p/s),lima,limb);
+}
+
 #pragma glslify:export(opRepLim)
